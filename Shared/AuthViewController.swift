bb1dbdfd0eb94324061fd4aa5a45bf31b4f45456
Seunghun Shin <18shshin@gmail.com>
2020-01-24 08:08:01 +0900

    deleted unnecessary files

---
+++ Shared/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SomeViewController.swift
+//  Shared
+//
+//  Created by Seunghun Shin on 2020/01/04.
+//  Copyright © 2020 SeunghunShin. All rights reserved.
+//
+
+import UIKit
+
+class SomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.navigationItem.setHidesBackButton(true, animated: false)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
