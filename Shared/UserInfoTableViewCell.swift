1295c33b9425c55ea50ef250545b06f49543caff
Seunghun Shin <18shshin@gmail.com>
2020-01-24 09:28:42 +0900

    친구 목록 View 추가 및 TableviewCell Class 추가

---
+++ Shared/UserInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoTableViewCell.swift
+//  Shared
+//
+//  Created by Seunghun Shin on 2020/01/24.
+//  Copyright © 2020 SeunghunShin. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
