f5ef886ab6f8a550a1d03bae55bda046845443c8
Seunghun Shin <18shshin@gmail.com>
2020-02-11 23:42:27 +0900

    [UPDATE] BankList 업데이트

--- Shared/Model/model.swift
+++ Shared/Model/model.swift
@@ -22,6 +22,6 @@ import Foundation
 
 public var myName : String?
 
-let banklist : [String] = ["씨티", "IBK기업", "KDB산업", "KB국민" , "NH농협", "신한", "외환" , "우리" , "SC제일"]
+let banklist : [String] = ["NH농협", "KB국민", "신한", "외환", "우리", "하나", "IBK기업", "외한", "SC제일", "KDB산업", "SBI저축은행", "새마을", "대구", "광주", "우체국", "신협", "전북", "경남", "부산", "수협", "제주", "케이뱅크"]
 
 let bankimagelist : [String] = ["citi", "IBK", "KDB", "kookmin", "nonghyub", "shinhan", "whyhan", "woori", "zeil"]
