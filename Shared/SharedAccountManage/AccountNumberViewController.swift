d55b5079680670b66511227fcb48ed81a15ac5f6
Seunghun Shin <18shshin@gmail.com>
2020-02-12 02:58:14 +0900

    [ADD] alert 기능 추가

--- Shared/SharedAccountManage/AccountNumberViewController.swift
+++ Shared/SharedAccountManage/AccountNumberViewController.swift
@@ -33,6 +33,23 @@ class AccountNumberViewController: UIViewController {
         if let uid = Auth.auth().currentUser?.uid, let bankName = bankLabel.text, let account = AccNumberField.text {
             ref?.child("Accounts/\(uid)").updateChildValues([bankName: account])
         }
-        
+        AccNumberField.text = ""
+        alertWithHome(message: "계좌가 정상적으로 연동 되었습니다.")
+    }
+}
+
+extension AccountNumberViewController {
+    func alertWithHome(title: String = "알림", message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "확인", style: .default) { (action) in
+            if #available(iOS 13.0, *) {
+                let vc = self.storyboard?.instantiateViewController(identifier: "mainUI")
+                self.navigationController?.pushViewController(vc!, animated: true)
+            } else {
+                fatalError()
+            }
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
