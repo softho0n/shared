be4957844b4bbf07d8746743fdda7c13b51ecbb2
Seunghun Shin <18shshin@gmail.com>
2020-02-15 11:14:06 +0900

    [ADD] 1,000,000원과 같은 표기를 위한 withComma 추가

--- Shared/extension.swift
+++ Shared/extension.swift
@@ -34,3 +34,14 @@ extension UIViewController {
         present(alert, animated: true, completion: nil)
     }
 }
+
+extension Int {
+    var withComma: String {
+        let decimalFormatter = NumberFormatter()
+        decimalFormatter.numberStyle = NumberFormatter.Style.decimal
+        decimalFormatter.groupingSeparator = ","
+        decimalFormatter.groupingSize = 3
+         
+        return decimalFormatter.string(from: self as NSNumber)!
+    }
+}
