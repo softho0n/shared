9033f1652ffa7a6537a0cf0d11095faf5abc2666
Seunghun Shin <18shshin@gmail.com>
2020-02-17 00:22:55 +0900

    [ADD] 더치페이 해당 사람 선택 Class 추가

--- Shared/SharedDutch/MakingGroupViewController.swift
+++ Shared/SharedDutch/MakingGroupViewController.swift
@@ -20,21 +20,36 @@ class MakingGroupViewController: UIViewController {
     var ref: DatabaseReference!
     var checkedCount = 0
     var myFiendList = [myFriendInfoStruct]()
+    var filteredList = [myFriendInfoStruct]()
     
     @IBOutlet var loader: UIActivityIndicatorView!
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupBackButton()
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
         tableView.cellLayoutMarginsFollowReadableWidth = false
         tableView.tableFooterView = UIView()
-        loader.backgroundColor = UIColor.white
         ref = Database.database().reference()
+        getDate()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.checkedCount = 0
+        self.filteredList.removeAll()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(true)
+        resetChecks()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! DutchBalanceViewController
+        vc.groupInfoList = filteredList
+        vc.totalCount = checkedCount
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,15 +70,41 @@ class MakingGroupViewController: UIViewController {
                         }
                     }
                     self.loader.stopAnimating()
-                    self.loader.backgroundColor = UIColor.clear
+                    print(self.myFiendList)
                     self.tableView.reloadData()
                 })
             }
         }
     }
     
+    func resetChecks(){
+       for i in 0 ..< tableView.numberOfRows(inSection: 0){
+           let indexPath = IndexPath(row: i, section: 0)
+           if let cell = tableView.cellForRow(at: indexPath){
+               cell.accessoryType = .none
+           }
+       }
+    }
+    
+    func filterData() {
+        for i in 0 ..< tableView.numberOfRows(inSection: 0){
+            let indexPath = IndexPath(row: i, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath){
+                if(cell.accessoryType == .checkmark) {
+                    self.checkedCount += 1
+                    self.filteredList.append(self.myFiendList[i])
+                }
+            }
+        }
+    }
     
     @IBAction func makeGroupBtn(_ sender: Any) {
+        filterData()
+        if(self.checkedCount == 0) {
+            self.alert(message: "한 명 이상 선택해주세요.")
+        } else {
+            self.performSegue(withIdentifier: "segueForSetMoney", sender: nil)
+        }
     }
 }
 
@@ -73,8 +114,20 @@ extension MakingGroupViewController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userInfoCell", for: indexPath) as! UserInfoTableViewCell
+        
+        cell.userNameLabel.text = self.myFiendList[indexPath.row].userName
+        cell.userPhoneNumberLabel.text = self.myFiendList[indexPath.row].userPhoneNum
+        cell.signInDateLabel.text = self.myFiendList[indexPath.row].userDate
+        return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+    }
 }
