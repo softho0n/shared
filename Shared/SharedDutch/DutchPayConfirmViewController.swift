6d9fc33192ec137ca55ea93fd7a4c20cd1b9afd9
Mun <eekdrnf9904@nate.com>
2020-02-22 04:07:44 +0900

    [UPDATE] DB에 멤버 수 등록 업데이트

--- Shared/SharedDutch/DutchPayConfirmViewController.swift
+++ Shared/SharedDutch/DutchPayConfirmViewController.swift
@@ -89,6 +89,7 @@ class DutchPayConfirmViewController: UIViewController {
                     groupdic.updateValue(myName!, forKey: "GroupBy")
                     groupdic.updateValue(self.groupName.text!, forKey: "GroupName")
                     groupdic.updateValue(self.dutchBalance, forKey: "TotalMoney")
+                    groupdic.updateValue("\(self.receiveGroupInfo.count)", forKey: "NumOfMembers")
                 }
                 
                 self.ref.child("ReceiveMetaData/\(uid)/\(uniquekey)").child("GroupInfo").setValue(groupdic)
