fe27370cc37d5597f6f8388d3b211a9a6cf61edd
Mun <eekdrnf9904@nate.com>
2020-03-31 16:25:21 +0900

    [UPDATE] 받기뷰에서 그룹생성하는 뷰 들어가는 기능 추가

---
+++ Shared/LogoView/UIView+Constraints.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+Constraints.swift
+//  Shared
+//
+//  Created by softhoon on 2020/03/31.
+//  Copyright © 2020 SeunghunShin. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pinEdgesToSuperView() {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leftAnchor.constraint(equalTo: superView.leftAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        rightAnchor.constraint(equalTo: superView.rightAnchor).isActive = true
+    }
+    
+}
